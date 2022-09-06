f23f9a6a1ed2ac6988bee3c9eeaa6aed84fe674b
朱建业 <borisdunk@sina.com>
2022-09-06 22:03:21 +0800

    解决相对路径报错问题

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-import {readFile} from 'fs';
-
-readFile('./corpus/data.json', (err, data) => {
-  if(!err) {
-    console.log(data.toString('utf-8'));
-  } else {
-    console.error(err);
-  }
-});
+const url = import.meta.url; // 获取当前脚本文件的url
+console.log('url: ' + url);
+const path = resolve(dirname(fileURLToPath(url)), 'corpus/data.json');
+console.log('path: ' + path);
+const data = readFileSync(path, { encoding: 'utf-8' });
+console.log(data);
